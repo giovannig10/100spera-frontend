43ccc91ee6310181bef6c1c4eae3f9810a80ae2b
vinicius valverde <vinicius.valverde@aluno.senai.br>
2025-11-18 09:08:35 -0300

    feat: create project

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+    return (
+        <main className={styles.main}>
+            <h1>Welcome to 100spera!</h1>
+        </main>
+    )
+}
