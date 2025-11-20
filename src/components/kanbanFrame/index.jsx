21eaeccd5e11638c92f18bb086f3c54753e5e587
Pedro Santos <pedro.o.santos7@aluno.senai.br>
2025-11-20 16:30:38 -0300

    feat: create orderItems orderCard

--- src/components/kanbanFrame/index.jsx
+++ src/components/kanbanFrame/index.jsx
@@ -8,13 +8,19 @@ import OrderCard from "../orderCard/index.jsx";
 
 export default function KanbanFrame() {
   const ordersUrl = "http://localhost:4000/100spera/orders";
+  const orderItemsUrl = "http://localhost:4000/100spera/order-items";
+
   const [orders, setOrders] = useState([]);
+  const [orderItems, setOrderItems] = useState([]);
 
   useEffect(() => {
     async function fetchOrders() {
       try {
-        const response = await axios.get(ordersUrl);
-        setOrders(response.data);
+        const ordersResponse = await axios.get(ordersUrl);
+        setOrders(ordersResponse.data);
+
+        const itemsResponse = await axios.get(orderItemsUrl);
+        setOrderItems(itemsResponse.data);
       } catch (error) {
         console.error("Error fetching orders:", error);
       }
@@ -28,7 +34,7 @@ export default function KanbanFrame() {
       <div className={styles.kanbanColumnsContainer}></div>
       <section className={styles.kanbanColumn}>
         {orders.map((order) => (
-          <OrderCard key={order.id} order={order} />
+          <OrderCard key={order.id} order={order} orderItems={orderItems}/>
         ))}
       </section>
       <section className={styles.kanbanColumn}></section>
--- src/components/orderCard/index.jsx
+++ src/components/orderCard/index.jsx
@@ -1,9 +1,30 @@
 import styles from "./orderCard.module.css";
 
-export default function OrderCard({ order }) {
+export default function OrderCard({ order, orderItems }) {
+
+    //receber pedido;
+    // receber items de pedido;
+    // filtrar na tabela de items pedido o id do pedido
+
+    const orderItem = orderItems.map((oi) => oi.orderId === order.id)
     return (
         <div key={order.id} className={styles.card}>
-            <h2 className={styles.cardTitle}>Order #{order.id}</h2>
+            <h1 className={styles.orderDetails}>Mesa {order.tableNumber}</h1>
+            {orderItems.map((item) => (
+                <div key={item.id} className={styles.orderItemContainer}>
+                    <div className={styles.itemsBox}>
+                        <p className={`${styles.itemDetails} ${styles.itemQuantity}`}>
+                            -{item.quantity}x
+                        </p>
+                        <p className={`${styles.itemDetails} ${styles.itemDishName}`}>
+                            {item.dish.name}
+                        </p>
+                    </div>
+                    <p className={`${styles.itemDetails} ${styles.itemObservations}`}>
+                        {item.observations}
+                    </p>
+                </div>
+            ))}
         </div>
     );
 }
