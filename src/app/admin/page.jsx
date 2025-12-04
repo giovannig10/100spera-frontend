cdaabac86b137fe7b7c3025d106607a4a02b28a9
Gabriela Fernanda <gabriela.f.barbosa@aluno.senai.br>
2025-12-04 10:00:15 -0300

    feat: create items section

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -10,13 +10,31 @@ export default function AdminPage() {
                     <h1 className={styles.title}>Funcionários</h1>
                 </div>
 
-                <div className={styles.categoryContainer}>
+                <div className={styles.infosContainer}>
                     <p className={styles.categoryTitle}>Entrada</p>
                     <p className={styles.categoryTitle}>Entrada</p>
                     <p className={styles.categoryTitle}>Entrada</p>
                     <p className={styles.categoryTitle}>Entrada</p>
                     <p className={styles.categoryTitle}>Entrada</p>
                 </div>
+
+                <div className={styles.infosContainer}>
+                    <p className={styles.dishTitle}>Nome</p>
+                    <p className={styles.dishTitle}>Imagem</p>
+                    <p className={styles.dishTitle}>Descrição</p>
+                    <p className={styles.dishTitle}>Preço</p>
+                    <p className={styles.dishTitle}>Ações</p>
+                </div>
+
+                <div className={styles.itemContainer}>
+                    <p className={styles.item}>Btata frita com cheddar</p>
+                    {/* <Image /> */}
+                    <button>Ver descrição</button>
+                    <p className={styles.item}>R$ 29,90</p>
+                    <button>Editar</button>
+                    <button>Excluir</button>
+                </div>
+
             </div>
                 <button className={styles.button}>
                     <p className={styles.buttonText}>Adicionar </p> <span className={styles.emoji}>+</span>
